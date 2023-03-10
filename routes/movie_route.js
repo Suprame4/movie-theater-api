ee4d534befc0174f5a51475a8c3ab4b0133e0634
Suprame4 <adriancortesgonz@gmail.com>
2023-03-09 18:06:27 -0800

    complete server side validation for show status and rating

--- routes/movie_route.js
+++ routes/movie_route.js
@@ -77,29 +77,59 @@ router.get('/shows/genre/:id', async (req, res) => {
 })
 
 //PUT update rating of a show that has been watched
-router.put('/shows/:id/users/:userId', async (req, res) => {
-    const shows = await Show.findOne({
-        where: {
-            id: req.params.id,
-            userId: req.params.userId
-        }
-    })
-    //find individual show 
-    console.log("SHOWS: ", shows)
-    //const show = shows.filter(show => show.id == req.params.id)
 
-    await shows.update({rating: req.body.rating})
+//update show rating 
+/*
+    Use server-side validation in your routes to ensure that:
+    If a user tries to make a request to update the rating of a show, 
+    the “rating” field cannot be empty or contain whitespace
+*/
+router.put('/shows/:id/users/:userId', [check("rating").not().isEmpty().trim()], 
+async (req, res) => {
     
-    res.json(shows)
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        res.json({error: errors.array()})
+    }
+    else{
+        const shows = await Show.findOne({
+            where: {
+                id: req.params.id,
+                userId: req.params.userId
+            }
+        })
+        //find individual show 
+        console.log("SHOWS: ", shows)
+        //const show = shows.filter(show => show.id == req.params.id)
+
+        await shows.update({rating: req.body.rating})
+        
+        res.json(shows)
+    }
 })
 
 //PUT update the status of a show
-router.put('/shows/:id', async (req, res) => {
-    //status either: on-going or cancelled 
-    const show = await Show.findByPk(req.params.id)
 
-    await show.update({status: req.body.status})
-    res.json(show)
+//updating show status
+    //Use server-side validation in your routes to ensure that:
+    //If a user tries to make a request to update the status of a show, 
+    //  the “status” field cannot be empty or contain whitespace.
+    //The status must be a minimum of 5 characters and a maximum of 25 characters
+router.put('/shows/:id', [check("status").not().isEmpty().trim().isLength({min: 5, max: 25})], async (req, res) => {
+    
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+
+        res.json({error: errors.array()})
+    }else {
+        //status either: on-going or cancelled 
+        const show = await Show.findByPk(req.params.id)
+
+        await show.update({status: req.body.status})
+        res.json(show)
+    }
 })
 
 
